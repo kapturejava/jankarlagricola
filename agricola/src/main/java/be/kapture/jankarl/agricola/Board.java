8faf434bdec553622277570021708356378834be
karlmis <karl.missiaen@kapture.be>
2019-04-12 16:17:02 +0200

    to 2 lists: completed

--- agricola/src/main/java/be/kapture/jankarl/agricola/Board.java
+++ agricola/src/main/java/be/kapture/jankarl/agricola/Board.java
@@ -3,7 +3,9 @@ package be.kapture.jankarl.agricola;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Board {
@@ -21,41 +23,49 @@ public class Board {
             GoodAction.create(GoodType.FOOD, 1)
     );
 
-    private final List<GoodAction> goodActions;
+    private List<GoodAction> replenishableActions;
+    private List<GoodAction> notReplenishableActions;
 
-    private Board(List<GoodAction> goodActions) {
-        this.goodActions = new ArrayList<>(goodActions);
+
+    public Board(List<GoodAction> replenishableActions, List<GoodAction> notReplenishableActions) {
+        this.replenishableActions = replenishableActions;
+        this.notReplenishableActions = notReplenishableActions;
     }
 
     public static Board create(int nrOfPlayers) {
-        return new Board(Lists.newArrayList(Iterables.concat(GOOD_ACTIONS_START_1_PLAYER_REPLENISHABLE, GOOD_ACTIONS_START_1_PLAYER_NOT_REPLENISHABLE)));
+        return new Board(GOOD_ACTIONS_START_1_PLAYER_REPLENISHABLE, GOOD_ACTIONS_START_1_PLAYER_NOT_REPLENISHABLE);
     }
 
     public List<GoodAction> getGoodActions(){
-        return goodActions;
+        return Lists.newArrayList(Iterables.concat(replenishableActions, notReplenishableActions));
     }
 
     public Board fetchAction(GoodAction goodAction){
-        List<GoodAction> remainingGoodActions = goodActions
+        List<GoodAction> newReplenishableActions = replenishableActions
                 .stream()
                 .filter(g -> !g.equals(goodAction))
                 .collect(Collectors.toList());
 
-        return new Board(remainingGoodActions);
+        List<GoodAction> newNotReplenishableActions = notReplenishableActions
+                .stream()
+                .filter(g -> !g.equals(goodAction))
+                .collect(Collectors.toList());
+
+        return new Board(newReplenishableActions, newNotReplenishableActions);
     }
 
     public Board nextTurn(){
 
-        List<GoodAction> newGoodActions = GOOD_ACTIONS_START_1_PLAYER_REPLENISHABLE.stream()
+        List<GoodAction> newReplenashables = GOOD_ACTIONS_START_1_PLAYER_REPLENISHABLE.stream()
                 .map(this::toNewGoodAction)
                 .collect(Collectors.toList());
-        newGoodActions.addAll(GOOD_ACTIONS_START_1_PLAYER_NOT_REPLENISHABLE);
 
-        return new Board(new ArrayList<>(newGoodActions));
+
+        return new Board(newReplenashables, GOOD_ACTIONS_START_1_PLAYER_NOT_REPLENISHABLE);
     }
 
     private GoodAction toNewGoodAction(GoodAction a) {
-        Optional<GoodAction> matchingGoodAction = goodActions.stream()
+        Optional<GoodAction> matchingGoodAction = replenishableActions.stream()
                 .filter(goodAction -> goodAction.getGoods().size() == 1)
                 .filter(goodAction -> extractSingleton(goodAction.getGoods().keySet()).equals(extractSingleton(a.getGoods().keySet())))
                 .findFirst();
